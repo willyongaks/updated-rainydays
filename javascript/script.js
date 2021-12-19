785d926965c7fb899efb99bd6ccd99a3cf025d21
willyongaks <83573335+willyongaks@users.noreply.github.com>
2021-12-19 17:17:17 +0300

    Add files via upload

---
+++ javascript/script.js
@@ -0,0 +1,45 @@
+const form = document.querySelector(".contact-form")
+const name = document.getElementById("name")
+const email = document.getElementById("email")
+const message = document.getElementById("message")
+
+
+// form.onsubmit = function () {
+//     console.log(event)
+// }
+
+function addToCart(id, image, name, price) {
+    let cart = localStorage.getItem("cart");
+    let parseCart = cart ? JSON.parse(cart) : [];
+    const checkCart = parseCart.filter(item => item.id === id);
+
+    console.log({ checkCart });
+    if(checkCart.length > 0) {
+        parseCart.map(item => {
+            if(id === item.id) {
+                item.quantity = item.quantity + 1;
+                item.total = item.quantity * item.price;
+                return item
+            }
+        })
+        console.log({ parseCart })
+        const data = JSON.stringify(parseCart)
+        localStorage.setItem("cart", data)
+    } else {
+        const product = {
+            id: id,
+            image: image,
+            name: name,
+            price: price,
+            quantity: 1,
+            total: price,
+        }
+        parseCart.push(product)
+        const data = JSON.stringify(parseCart)
+        console.log({ parseCart, data })
+        localStorage.setItem("cart", data)
+    }
+
+}
+
+
